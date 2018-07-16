c04ebae6f9114f9180f40ca3622765990771bd02
Sandy <sandytoh@u.nus.edu>
2018-07-16 11:01:51 +0800

    updated

---
+++ Group8_AD_webapp/model/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group8_AD_webapp.model
+{
+    public class Request
+    {
+        int reqId;
+        int empId;
+        int approverId;
+        string approverComment;
+        DateTime reqDateTime;
+        DateTime approvedDateTime;
+        DateTime cancelledDateTime;
+        string status;
+
+
+    }
+}
