d00b9fa015ebcc27a946e1476a44444dcc060517
Sandy <sandytoh@u.nus.edu>
2018-07-18 09:39:12 +0800

    Changes to Main

--- Group8_AD_webapp/Main.Master.cs
+++ Group8_AD_webapp/Main.Master.cs
@@ -11,13 +11,32 @@ namespace Group8_AD_webapp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            List<Item> items = new List<Item>();
+            if (!IsPostBack)
+            {
+                items.Add(new Item("C007", "Stapler", "Stapler 1in", 150, 1.50, "box"));
+                items.Add(new Item("A002", "Pen", "Pencil 2B, With Eraser End", 50, 1.02, "pack of 12"));
+                items.Add(new Item("B054", "File", "File, Blue", 100, 1.23, "each"));
+                items.Add(new Item("C008", "Stapler", "Stapler 2in", 150, 1.50, "box"));
+            }
+            lstCart.DataSource = items;
+            lstCart.DataBind();
         }
 
         public void ShowToastr(object sender, string message, string title, string type)
         {
             ScriptManager.RegisterStartupScript((Page)sender, sender.GetType(), "toastr_message",
             String.Format("toastr.{0}('{1}', '{2}', {{positionClass: 'toast-bottom-right'}});", type.ToLower(), message, title), true);
+        }
+
+        protected void lstCart_PagePropertiesChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRemove_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Group8_AD_webapp/Main.Master.designer.cs
+++ Group8_AD_webapp/Main.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Group8_AD_webapp {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// lstCart control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView lstCart;
+        
+        /// <summary>
         /// UpdatePanel1 control.
         /// </summary>
         /// <remarks>
