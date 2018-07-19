abc4cdf5d5fa783594d328375f45185e6254522e
Sandy <sandytoh@u.nus.edu>
2018-07-19 16:09:27 +0800

    Main - Cart Button

--- Group8_AD_webapp/Main.Master.cs
+++ Group8_AD_webapp/Main.Master.cs
@@ -4,29 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Group8_AD_webapp.Models;
 
 namespace Group8_AD_webapp
 {
     public partial class Main : System.Web.UI.MasterPage
     {
+        static string access_token;
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<Item> items = new List<Item>();
             if (!IsPostBack)
             {
-                items.Add(new Item("C007", "Stapler", "Stapler 1in", 150, 1.50, "box"));
-                items.Add(new Item("A002", "Pen", "Pencil 2B, With Eraser End", 50, 1.02, "pack of 12"));
-                items.Add(new Item("B054", "File", "File, Blue", 100, 1.23, "each"));
-                items.Add(new Item("C008", "Stapler", "Stapler 2in", 150, 1.50, "box"));
+                FillCart();
             }
-            lstCart.DataSource = items;
-            lstCart.DataBind();
         }
 
         public void ShowToastr(object sender, string message, string title, string type)
         {
             ScriptManager.RegisterStartupScript((Page)sender, sender.GetType(), "toastr_message",
             String.Format("toastr.{0}('{1}', '{2}', {{positionClass: 'toast-bottom-right'}});", type.ToLower(), message, title), true);
+        }
+
+        public void FillCart()
+        {
+            int empId = 31;
+            RequestVM cart = Controllers.RequestCtrl.GetReq(empId, "Unsubmitted", access_token).FirstOrDefault();
+            List<RequestDetailVM> cartDetailList = new List<RequestDetailVM>();
+
+            if (cart != null)
+            {
+                int reqId = cart.ReqId;
+                List<RequestDetailVM> reqDetails = Controllers.RequestDetailCtrl.GetReqDetList(reqId, access_token);
+                reqDetails = BusinessLogic.AddItemDescToReqDet(reqDetails);
+                cartDetailList = reqDetails;
+                lstCart.DataSource = cartDetailList;
+                lstCart.DataBind();
+
+                lblCartCount.Text = cartDetailList.Count().ToString();
+            }
+            else {
+                lstCart.DataSource = cartDetailList;
+                lstCart.DataBind();
+            }
         }
 
         protected void lstCart_PagePropertiesChanged(object sender, EventArgs e)
--- Group8_AD_webapp/Main.Master.designer.cs
+++ Group8_AD_webapp/Main.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Group8_AD_webapp {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// lblCartCount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCartCount;
+        
+        /// <summary>
         /// lstCart control.
         /// </summary>
         /// <remarks>
