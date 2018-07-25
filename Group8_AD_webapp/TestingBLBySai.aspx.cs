2fd39e59425065d4597559dc7c632fafb059d64f
saiminhtet <htetsaimin@gmail.com>
2018-07-25 10:40:02 +0800

    added Testing webpage

---
+++ Group8_AD_webapp/TestingBLBySai.aspx.cs
@@ -0,0 +1,25 @@
+﻿using Group8_AD_webapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Group8AD_WebAPI.BusinessLogic;
+namespace Group8_AD_webapp
+{
+    public partial class TestingBL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            List<ItemVM> iList = ItemBL.GetEmpItems(4);
+            GridView1.DataSource = iList;
+            GridView1.DataBind();
+        }
+    }
+}
